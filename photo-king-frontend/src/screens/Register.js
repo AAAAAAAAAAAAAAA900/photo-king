f02747abeb79efded161f4918052c9071107bf29
Brandon Perez <przbrandon1212@gmail.com>
2025-02-03 13:05:25 -0500

    Register merge and added react hook package

--- photo-king-frontend/src/screens/Register.js
+++ photo-king-frontend/src/screens/Register.js
@@ -1,17 +1,20 @@
-import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity, Button } from 'react-native';
 import { useState } from 'react';
 import styles from '../styles/ComponentStyles.js';
 import axios from 'axios';
+import { useForm, Controller } from 'react-hook-form';
 
-export default function RegisterScreen ({navigation}){
-  // Register screen logic: store username and password
-  const [username, setUsername] = useState(""); // State for username
-  const [password, setPassword] = useState(""); // State for password
-  
-  //Register attempt
+export default function RegisterScreen ({navigation}){  
+
+
+  const { control, handleSubmit, errors } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  }
+
+  // Send Register attempt to backend
   const Register = async () => {
-
-    // Send login attempt to backend
     try {
       const response = await axios.post('https://1785-2600-4040-af73-4a00-98d9-4725-d184-8b0.ngrok-free.app/api/user/register', {username: username, password: password},
         {
@@ -31,9 +34,25 @@ export default function RegisterScreen ({navigation}){
   // Login screen view
   return(
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={Login}>
-        <Text>Register</Text>
-      </TouchableOpacity>
+      <View>
+        <Text>Username</Text>
+        <Controller
+          name="username"
+          control={control}
+          render={(props) => <TextInput {...props}/>}
+          rules={{ required: "This field is required" }}
+        />
+      </View>
+      <View>
+        <Text>Password</Text>
+        <Controller
+          name="password"
+          control={control}
+          render={(props) => <TextInput {...props}/>}
+          rules={{ required: "This field is required" }}
+        />
+      </View>
+      <Button title="Register" onPress={handleSubmit(onSubmit)} />
     </SafeAreaView>
   );
 }
