5c036d54245571878b3334af3c5ccd5467ba278b
Brandon Perez <przbrandon1212@gmail.com>
2025-02-02 19:53:08 -0500

    Added register page and implemented input component class
    Co-authored-by: stackfish <stackfish@users.noreply.github.com>
    Co-authored-by: Connor Strutt <AAAAAAAAAAAAAAA900@users.noreply.github.com>

---
+++ photo-king-frontend/src/components/Input.js
@@ -0,0 +1,31 @@
+import { Component } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+
+export default class Input extends Component {
+    constructor(){
+        super();
+    }
+    render() {
+      return (
+        <SafeAreaView style={styles.container}>
+            <Text margin={10}>Username</Text>
+            <TextInput
+                style={styles.textIn}
+                onChangeText={this.props.userUpdate}
+                autoCapitalize ='none'
+                autoCorrect ={false}
+                placeholder="Enter username"
+            />
+            <Text margin={10}>Password</Text>
+            <TextInput
+                style={styles.textIn}
+                onChangeText={this.props.passUpdate}
+                autoCapitalize ='none'
+                autoCorrect ={false}
+                placeholder="Enter password"
+                secureTextEntry={true}
+            />
+        </SafeAreaView>
+      );
+    }
+}
