ef13eda06d7052143ee4a1ecebc375bbeb8cf6d7
Brandon Perez <przbrandon1212@gmail.com>
2025-02-04 11:06:21 -0500

    Fixed minor bug in register.js

--- photo-king-frontend/src/api/utils.js
+++ photo-king-frontend/src/api/utils.js
@@ -1,2 +1,2 @@
 
-export const API_URL = 'https://9b10-2600-4040-af73-4a00-f4d5-ce24-c68a-bf82.ngrok-free.app';
+export const API_URL = 'https://1362-2600-4040-af73-4a00-5198-711b-62cd-e95.ngrok-free.app';
