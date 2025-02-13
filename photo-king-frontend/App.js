fc2aa3f22ff3d0f6d0db6311eeffa160fcc1f53b
AAAAAAAAAAAAAAA900 <cj2strutt@gmail.com>
2025-02-13 12:16:15 -0500

    Removed back buttons from navbar screens

--- photo-king-frontend/App.js
+++ photo-king-frontend/App.js
@@ -10,7 +10,7 @@ import RegisterScreen from './src/screens/Register.js';
 import HomeScreen from './src/screens/Home.js';
 import GroupScreen from './src/screens/Group.js';
 import TitleButtons from './src/components/TitleButtons.js';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import DefaultText from './src/components/DefaultText.js';
 import ProfileScreen from './src/screens/Profile.js';
 import SettingsScreen from './src/screens/Settings.js';
@@ -49,41 +49,41 @@ export default function App() {
             <Stack.Navigator initialRouteName="Login">
               <Stack.Screen
                 name='Login'
-                options={{headerTitleStyle:styles.titleText, headerShown: false }}
+                options={{ headerShown: false }}
                 component={LoginScreen}
               />
               <Stack.Screen
                 name='Register'
-                options={{headerTitleStyle:styles.titleText}}
+                options={headerStyles.default}
                 component={RegisterScreen}
               />
               <Stack.Screen
                 name='Home'
                 options={({ navigation }) => ({
-                  // headerTitleStyle:styles.titleText,
-                  headerTitle: () =><DefaultText style={styles.titleText}>Home</DefaultText>,
                   headerRight: () => (<TitleButtons navigation={navigation}/>),
+                  headerTitleStyle:styles.titleText,
+                  headerBackVisible:false
               })}
                 component={HomeScreen}
               />
               <Stack.Screen
                 name='Group'
-                options={{headerTitleStyle:styles.titleText}}
+                options={headerStyles.default}
                 component={GroupScreen}
               />
               <Stack.Screen
                 name='Profile'
-                options={{headerTitleStyle:styles.titleText}}
+                options={headerStyles.noBack}
                 component={ProfileScreen}
               />
               <Stack.Screen
                 name='Settings'
-                options={{headerTitleStyle:styles.titleText}}
+                options={headerStyles.default}
                 component={SettingsScreen}
               />
               <Stack.Screen
                 name='Friends'
-                options={{headerTitleStyle:styles.titleText}}
+                options={headerStyles.noBack}
                 component={FriendsScreen}
               />
             </Stack.Navigator>
@@ -93,3 +93,12 @@ export default function App() {
   );
 }
 
+const headerStyles = StyleSheet.create({
+  default:{
+    headerTitleStyle:styles.titleText,
+  },
+  noBack: {
+    headerTitleStyle:styles.titleText,
+    headerBackVisible:false
+  }
+});
