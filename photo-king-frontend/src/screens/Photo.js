1aa4093c2a9890e35be1f280e0269302637852cb
AAAAAAAAAAAAAAA900 <cj2strutt@gmail.com>
2025-02-17 15:25:40 -0500

    photo page UI template: buttons and comment bar

--- photo-king-frontend/src/screens/Photo.js
+++ photo-king-frontend/src/screens/Photo.js
@@ -1,5 +1,7 @@
 import { useRoute } from "@react-navigation/native";
-import { Image, SafeAreaView } from "react-native";
+import { FlatList, Image, SafeAreaView, TextInput, TouchableOpacity, View } from "react-native";
+import styles, { colors } from "../styles/ComponentStyles";
+import DefaultText from "../components/DefaultText";
 
 
 
@@ -9,16 +11,36 @@ export default function PhotoScreen ({navigation}){
     const group = route.params?.group;
     const photo = route.params?.photo;
 
-    console.log(user);
-    console.log(group);
-    console.log(photo);
-
     return(
         <SafeAreaView style={{flex:1}}>
-            <Image 
-            source={{uri: photo.uri}}
-            style={{flex:1, maxHeight:'60%', maxWidth:'100%'}}
-            />
+            <View style={{flex:1, maxHeight:'60%', maxWidth:'100%', backgroundColor:colors.grey }}>
+                <Image 
+                source={{uri: photo.uri}}
+                style={{height:'100%', width:'100%', resizeMode:'contain'}}
+                />
+            </View>
+            <View borderWidth={1} style={{padding: 20, flexDirection:'row', backgroundColor:colors.primary}}>
+                <TouchableOpacity
+                onPress={()=>{}}
+                style={[styles.button, {backgroundColor: colors.secondary}]}
+                >
+                    <DefaultText>Like</DefaultText>
+                </TouchableOpacity>
+                <TouchableOpacity
+                onPress={()=>{}}
+                style={[styles.button, {backgroundColor: colors.secondary}]}
+                >
+                    <DefaultText>Download</DefaultText>
+                </TouchableOpacity>
+            </View>
+            <View style={{flex:1}}>
+                <FlatList/>
+                <TextInput
+                style={[styles.textIn, {margin:20}]}
+                onChangeText={()=>{}}
+                placeholder="Enter Comment..."
+                />
+            </View>
         </SafeAreaView>
     );
 }
