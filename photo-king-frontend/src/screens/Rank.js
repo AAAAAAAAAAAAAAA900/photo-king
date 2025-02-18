00473bb252678e2a3cd156a09be558e02694220c
AAAAAAAAAAAAAAA900 <cj2strutt@gmail.com>
2025-02-18 18:47:25 -0500

    fixed ranking page logic. Now ranks can be assigned/unassigned in any order

--- photo-king-frontend/src/screens/Rank.js
+++ photo-king-frontend/src/screens/Rank.js
@@ -10,35 +10,79 @@ export default function RankScreen({navigation}){
     const user = route.params?.user;
     const group = route.params?.group;
     const [pictures, setPictures] = useState([]);
-    
+    const [ranks, setRanks] = useState({});  //tracks image rankings by url
 
     // useEffect to get group pictures on load
     useEffect(() => {
         loadPictures(setPictures, group).then(r => {});
     }, []);
 
+    const rankPhoto = (url) =>{
+        if(ranks[url] != undefined){
+            // If already ranked, undo rank
+            const {[url]:_, ...copy} = ranks;
+            setRanks(copy);
+        } else if (Object.keys(ranks).length < 3 ){
+            // If there are available rankings, find them
+            const openings = [true, true, true];
+            for(const url in ranks){
+                openings[ranks[url]] = false;
+            }
+            // Take the lowest available ranking
+            for(let i = 0; i < 3; ++i){
+                if(openings[i]){
+                    const copy = {...ranks, [url]:i};
+                    setRanks(copy);
+                    break;
+                }
+            }
+        }
+    };
+
     // FlatList element's view
     const RankablePic = ({ photo }) => {
+        const imageRank = ranks[photo.url] ?? null;
         return (
             <TouchableOpacity 
-            onPress={()=>{console.log("ranking an image");}}
+            onPress={()=>{rankPhoto(photo.url);}}
             style={styles.picHolder}>
                 <Image
                     style={styles.pic}
                     source={{uri: photo.url}}
                     // defaultSource= default image to display while loading images.
                 />
-                <DefaultText style={{position:'absolute', top:5, left:5}}>1</DefaultText>
+                { imageRank !== null &&
+                    <View style={{width:30, height:30, borderRadius:15, position:'absolute', top:5, left:5, backgroundColor:colors.primary, alignItems:'center', justifyContent: 'center'}}>
+                        <DefaultText>{imageRank+1}</DefaultText>
+                    </View>
+                }
             </TouchableOpacity>
         );
     };
 
     return(
         <SafeAreaView style={{flex:1}}>
+            <View style={{padding:10, height:50, backgroundColor:colors.secondary, flexDirection:'row', alignItems:'center'}}>
+                {!Object.values(ranks).includes(0) &&
+                    <View style={{width:30, height:30, borderRadius:15, backgroundColor:colors.primary, alignItems:'center', justifyContent: 'center'}}>
+                        <DefaultText>1</DefaultText>
+                    </View>
+                }
+                {!Object.values(ranks).includes(1) &&
+                    <View style={{width:30, height:30, borderRadius:15, backgroundColor:colors.primary, alignItems:'center', justifyContent: 'center'}}>
+                        <DefaultText>2</DefaultText>
+                    </View>
+                }
+                {!Object.values(ranks).includes(2) &&
+                    <View style={{width:30, height:30, borderRadius:15, backgroundColor:colors.primary, alignItems:'center', justifyContent: 'center'}}>
+                        <DefaultText>3</DefaultText>
+                    </View>
+                }
+            </View>
             <View style={{flex:1}}>
                 <FlatList 
                     numColumns={3}
-                    renderItem={({ item }) => <RankablePic photo={item} />}
+                    renderItem={({ item }) => <RankablePic photo={item}/>}
                     keyExtractor={(picture) => picture.url}
                     data={pictures}
                 />
